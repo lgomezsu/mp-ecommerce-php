fd4b3696b211232ef1e90e783a4a27d68e6c8d0c
lgomezsu <lgomezsu01@gmail.com>
2020-06-12 01:54:48 -0300

    activado php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-//<?php
+<?php
 // Cargar SDK de Mercado Pago
-//require __DIR__ .  '/vendor/autoload.php';
+require __DIR__ .  '/vendor/autoload.php';
 
 // Agregar credenciales Vendedor
 //MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
-//?>
+?>
 
 <html class="supports-animation supports-columns svg no-touch no-ie no-oldie no-ios supports-backdrop-filter as-mouseuser" lang="en-US"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     
