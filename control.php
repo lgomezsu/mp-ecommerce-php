4d7bb87e89a07218b7effaf93179383e415f8eb6
lgomezsu <lgomezsu01@gmail.com>
2020-06-12 10:45:29 -0300

    conf 2

---
+++ control.php
@@ -0,0 +1,10 @@
+
+
+<?php
+// Cargar SDK de Mercado Pago
+require __DIR__ .  '/vendor/autoload.php';
+
+// Agregar credenciales Vendedor
+//MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
+
+?>
