b24620665d56cf10de41ce805b8f615c94f704ab
lgomezsu <lgomezsu01@gmail.com>
2020-06-14 04:20:43 -0300

    ok

--- detail.php
+++ detail.php
@@ -2,10 +2,10 @@
 
 // SDK de Mercado Pago
 require_once __DIR__ .'/vendor/autoload.php';
-/*
+
 // Agrega credenciales
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
-
+/*
 // Codigo Integrador
 MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
